de011148d4fceaa452ef21b95e781e62b28868c2
Xamps0 <david_ribeiro_mg@icloud.com>
2025-09-05 10:48:50 -0300

    Add files via upload

--- Projeto Geração.py
+++ Projeto Geração.py
@@ -3,18 +3,11 @@ import random
 
 #Load/Carregar
 pygame.init()
+
 Janela = pygame.display.set_mode((640,480))
 pygame.display.set_caption("Projeto Geração")
 clock = pygame.time.Clock()
 FPS = 60
-
-
-posicoesX = [0] * 5
-posicoesY = [0] * 5
-
-for p in range(5):
-    posicoesX[p] = random.randrange(100,490)
-    posicoesY[p] = random.randrange(100,330)
 
 velocidadeX = [0] * 5
 velocidadeY = [0] * 5
@@ -22,6 +15,13 @@ velocidadeY = [0] * 5
 for v in range(5):
     velocidadeX[v] = random.randrange(-6,6)
     velocidadeY[v] = random.randrange(-6, 6)
+
+posicoesX = [0] * 5
+posicoesY = [0] * 5
+
+for p in range(5):
+    posicoesX[p] = random.randrange(100,490)
+    posicoesY[p] = random.randrange(100,330)
 
 cores = [(0,0,0)] * 5
 
@@ -40,12 +40,13 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    for v in range(5):
+       posicoesX[v] += velocidadeX[v]
 
     #Draw/Apresentação
     Janela.fill((0,0,0))
     for i in range(5):
-        pygame.draw.rect(Janela,(cores[i]),(posicoesX[i],posicoesY[i], 50,50 ))
+        pygame.draw.rect(Janela, (cores[i]), (posicoesX[i], posicoesY[i], 50, 50 ))
 
     pygame.display.update()
 
